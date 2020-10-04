cddb33970d02343a5717455429c65fcc423f5eec
nxtstep <code@supersimple.io>
2020-10-04 11:44:10 +0200

    Corrected inline documentation

--- Sources/BundleKit/Bundle+JSON.swift
+++ Sources/BundleKit/Bundle+JSON.swift
@@ -8,9 +8,8 @@ extension Bundle {
     ///   - file: the resource filename inside the bundle
     ///   - dateDecodingStrategy: the date decoding strategy. Default: `.deferredToDate`
     ///   - keyDecodingStrategy: the key decoding strategy. Default: `.useDefaultKeys`
-    /// - Throws: Bundle.Error
-    /// - Returns: the JSON parsed structure or fails with fatalError if the resource is malformed
-    ///             or not found in the bundle
+    /// - Throws: Bundle.Error if the resource is malformed or not found in the bundle
+    /// - Returns: the JSON parsed structure
     public func decode<T: Decodable>(
         _ type: T.Type, from file: String,
         dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
